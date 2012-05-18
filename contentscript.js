6dfc02bf390e5fdb7a451464d9622416c57bc8b7
lemonhall <lemonhall@gmail.com>
2012-05-18 21:11:22 +0800

    first commit

--- contentscript.js
+++ contentscript.js
@@ -3,19 +3,7 @@
  * source code is governed by a BSD-style license that can be found in the
  * LICENSE file.
  */
-var regex = /http:\/\/douban\.fm\/\?start.*cid=0/g;
-
-
-// Test the text of the body element against our regular expression.
-if (regex.test(document.body.innerText)) {
-  // The regular expression produced a match, so notify the background page.
-	var musiclist=new Array();
-		musiclist=document.body.innerText.match(regex);
-		chrome.extension.connect().postMessage(musiclist);
-		//location.href(musiclist[0]);
-		//alert(musiclist);
-} else {
-  // No match was found.
-  
-
-}
+(function(){
+	var subjs=$("a[href*='http://www.douban.com/subject/']");
+	subjs.parent().parent().parent().hide(); 
+} )();
